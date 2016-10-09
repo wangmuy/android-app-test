0c1f21f8594b3486aeacd49e6a80213813f892f5
wangmuy <wangmuy@gmail.com>
2016-10-09 17:43:49 +0800

    clipRect border test

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,18 +3,32 @@ package com.example.test;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private ViewGroup mRootView;
+    private View mClipRectView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRootView = (ViewGroup) findViewById(R.id.rootView);
+        mClipRectView = findViewById(R.id.clipView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+//        mClipRectView.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                mClipRectView.invalidate();
+//                mClipRectView.post(this);
+//            }
+//        });
     }
 }
