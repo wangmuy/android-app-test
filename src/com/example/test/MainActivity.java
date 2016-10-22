8ad3ac114c525e42522d193013491b2d24fddc7f
wangmuy <wangmuy@gmail.com>
2016-10-22 16:15:56 +0800

    eclipse empty project

---
+++ src/com/example/test/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
