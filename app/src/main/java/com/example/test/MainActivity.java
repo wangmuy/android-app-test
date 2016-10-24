9fa6a7e0c58be077a933b1501e9ecfb9a119f013
wangmuy <wangmuy@gmail.com>
2016-10-24 11:21:57 +0800

    crash java with delay

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.test;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private static final int DELAY_MILLIS = 0;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHandler = new Handler();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String msg = "Crashing this activity, with no survivers!";
+                Log.d(TAG, msg);
+                throw new RuntimeException(msg);
+            }
+        }, DELAY_MILLIS);
     }
 }
