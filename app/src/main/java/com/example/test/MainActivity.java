6e3f86c5a69c6c0185715d80bac5b4a4085efe36
wangmuy <wangmuy@gmail.com>
2016-11-30 15:19:50 +0800

    6.0 api23 runtime permission

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,20 +1,72 @@
 package com.example.test;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final int REQ_READ_EXTERNAL_STORAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            final String workStr = "doWork";
+            Log.d(TAG, workStr);
+            Toast.makeText(MainActivity.this, workStr, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @OnClick(R.id.reqperm_btn)
+    public void requestPermission() {
+        if(PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            mHandler.sendEmptyMessage(0);
+        } else {
+            Log.d(TAG, "request perm on runtime...");
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "humbly ask permission of READ_EXTERNAL_STORAGE", Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQ_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case REQ_READ_EXTERNAL_STORAGE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mHandler.sendEmptyMessage(0);
+                } else {
+                    final String logstr = "WTF you refused permission request";
+                    Log.d(TAG, logstr);
+                    Toast.makeText(this, logstr, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
