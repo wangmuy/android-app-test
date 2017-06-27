50186106e36694cbbf1cb4024d427de18abad871
wangmuy <wangmuy@gmail.com>
2017-06-27 15:31:51 +0800

    find template match

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -10,23 +11,29 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Point3;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    public static final String TAG = "MainActivity";
 
+    private String COPY_DIR_PATH;
     private Handler H = new Handler();
     private TextView mInfo;
     private ImageView mSrcImg;
     private ImageView mDstImg;
+    private Bitmap mSrcBmp;
+    private Bitmap mDstBmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,62 +44,128 @@ public class MainActivity extends AppCompatActivity {
         mSrcImg = (ImageView) findViewById(R.id.srcImg);
         mDstImg = (ImageView) findViewById(R.id.dstImg);
 
-        Log.d(TAG, "OpenCV load "+OpenCVUtils.init());
+        COPY_DIR_PATH = getCacheDir() + "/assets/";
+        Log.d(TAG, "OpenCV load " + OpenCVUtils.init());
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                final String assetImg = "screenshot.png";
-                final String srcImg = getCacheDir()+"/screenshot.png";
-                final String dstImg = getCacheDir()+"/screenshot-canny.png";
-
                 // copy from assets to cache dir
-                try {
-                    InputStream is = getAssets().open(assetImg);
-                    FileOutputStream fos = new FileOutputStream(new File(srcImg));
-                    int len;
-                    byte[] buffer = new byte[1024];
-                    while((len=is.read(buffer)) > 0) {
-                        fos.write(buffer, 0, len);
-                    }
-                    fos.close();
-                    is.close();
-                } catch (IOException e) {
-                    Log.e(TAG, "", e);
+                File copyDir = new File(COPY_DIR_PATH);
+                if (!copyDir.exists()) {
+                    copyDir.mkdirs();
                 }
+                copyAssets(COPY_DIR_PATH);
 
-                // read
-                Mat image = Imgcodecs.imread(srcImg);
-                Log.d(TAG, "img w/h="+image.width()+"/"+image.height());
-
-                // canny edge
-                int threadshold1 = 70;
-                int threadshold2 = 100;
-                Mat im_canny = new Mat();
                 long beg = System.currentTimeMillis();
-                Imgproc.Canny(image, im_canny, threadshold1, threadshold2);
+//                testCV();
+                testFindTemplate();
                 final long opTime = System.currentTimeMillis() - beg;
-                boolean writeOk = Imgcodecs.imwrite(dstImg, im_canny);
-                Log.d(TAG, "canny op "+writeOk);
-
-                final Bitmap srcBmp = BitmapFactory.decodeFile(srcImg);
-                final Bitmap dstBmp = BitmapFactory.decodeFile(dstImg);
 
                 // show images
                 H.post(new Runnable() {
                     @Override
                     public void run() {
-                        mInfo.setText("Canny operation time: "+opTime);
-                        mSrcImg.setImageBitmap(srcBmp);
-                        mDstImg.setImageBitmap(dstBmp);
+                        final String info = "Canny operation time: " + opTime + "ms";
+                        Log.d(TAG, info);
+                        mInfo.setText(info);
+                        mSrcImg.setImageBitmap(mSrcBmp);
+                        mDstImg.setImageBitmap(mDstBmp);
                     }
                 });
             }
         }).start();
     }
 
+    private void testCV() {
+        final String srcImg = COPY_DIR_PATH + "screenshot.png";
+        final String dstImg = COPY_DIR_PATH + "screenshot-canny.png";
+        // read
+        Mat image = Imgcodecs.imread(srcImg);
+        Log.d(TAG, "img w/h=" + image.width() + "/" + image.height());
+
+        // canny edge
+        int threadshold1 = 70;
+        int threadshold2 = 100;
+        Mat im_canny = new Mat();
+
+        Imgproc.Canny(image, im_canny, threadshold1, threadshold2);
+        boolean writeOk = Imgcodecs.imwrite(dstImg, im_canny);
+        Log.d(TAG, "canny op " + writeOk);
+
+        mSrcBmp = BitmapFactory.decodeFile(srcImg);
+        mDstBmp = BitmapFactory.decodeFile(dstImg);
+    }
+
+    private void testFindTemplate() {
+        final String src = COPY_DIR_PATH + "screenshot.png";
+        final String sch = COPY_DIR_PATH + "wifi_w_pwd.png";
+        Mat srcImg = Imgcodecs.imread(src);
+        Mat schImg = Imgcodecs.imread(sch);
+        ArrayList<Point3> matches = OpenCVUtils.findAllTemplate(srcImg, schImg, 0.9, 8, false);
+        OpenCVUtils.markMatches(srcImg, schImg, matches);
+
+        mSrcBmp = BitmapFactory.decodeFile(src);
+        Bitmap mark = Bitmap.createBitmap(srcImg.cols(), srcImg.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(srcImg, mark);
+        mDstBmp = mark;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    /**
+     * copy assets, not including sub folders
+     * @param outDir
+     */
+    private void copyAssets(String outDir) {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to get asset file list.", e);
+        }
+        if (files != null) for (String filename : files) {
+            if(filename.equals("images") || filename.equals("sounds") || filename.equals("webkit")) {
+                Log.d(TAG, "skipping "+filename);
+                continue;
+            }
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open(filename);
+                File outFile = new File(outDir, filename);
+                out = new FileOutputStream(outFile);
+                copyFile(in, out);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to copy asset file: " + filename, e);
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+            }
+        }
+    }
+
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
     }
 }
--- app/src/main/java/com/example/test/OpenCVUtils.java
+++ app/src/main/java/com/example/test/OpenCVUtils.java
@@ -3,13 +3,22 @@ package com.example.test;
 import android.util.Log;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
 
 /**
  * Created by wangzhongdi on 2017/6/26.
  */
 
 public class OpenCVUtils {
-    private static final String TAG = "OpenCVUtils";
+    private static final String TAG = MainActivity.TAG;//"OpenCVUtils";
     private OpenCVUtils(){}
 
     static {
@@ -18,5 +27,82 @@ public class OpenCVUtils {
 
     public static boolean init() {
         return OpenCVLoader.initDebug();
+    }
+
+    /**
+     * Find all matches using template
+     * @param src
+     * @param sch
+     * @param threadshold
+     * @param maxCount
+     * @param bgRemove
+     * @return list of (match.x, match.y, match.confidence)
+     */
+    public static ArrayList<Point3> findAllTemplate(Mat src, Mat sch, double threadshold, int maxCount, boolean bgRemove) {
+        final int procMethod = Imgproc.TM_CCOEFF_NORMED;
+        Mat source;
+        Mat search;
+        Mat srcGray = new Mat();
+        Mat schGray = new Mat();
+        Imgproc.cvtColor(src, srcGray, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(sch, schGray, Imgproc.COLOR_BGR2GRAY);
+        if(bgRemove) {
+            // 边界提取(来实现背景去除的功能)
+            source = new Mat();
+            Imgproc.Canny(srcGray, source, 100, 200);
+            search = new Mat();
+            Imgproc.Canny(schGray, search, 100, 200);
+        } else {
+            source = srcGray;
+            search = schGray;
+        }
+
+        Mat result = new Mat();
+        Imgproc.matchTemplate(source, search, result, procMethod);
+
+        ArrayList<Point3> matchList = new ArrayList<>();
+        Scalar coverScalar = new Scalar(0, 0, 0);
+        while(true) {
+            final Core.MinMaxLocResult minMax = Core.minMaxLoc(result);
+            if (minMax.maxVal < threadshold) {
+                break;
+            }
+
+            Point topLeft; // current best match
+            if (procMethod == Imgproc.TM_SQDIFF || procMethod == Imgproc.TM_SQDIFF_NORMED) {
+                topLeft = minMax.minLoc;
+            } else {
+                topLeft = minMax.maxLoc;
+            }
+            matchList.add(new Point3(topLeft.x, topLeft.y, minMax.maxVal));
+            Log.d(TAG, "match: " + topLeft + " - " + new Point(topLeft.x + sch.cols(), topLeft.y + sch.rows()));
+            if(matchList.size() >= maxCount)
+                break;
+            // 遮掉当前最匹配的, 继续查找下一个匹配
+            Imgproc.rectangle(result, topLeft, new Point(topLeft.x+search.cols(), topLeft.y+search.rows()), coverScalar, -1);
+        }
+
+        if(matchList.size() == 0)
+            Log.d(TAG, "no best match: threadshold=" + threadshold);
+        return matchList;
+    }
+
+    /**
+     * Mark all matches using red rectangle
+     * @param source
+     * @param search
+     * @param matchList
+     */
+    public static void markMatches(Mat source, Mat search, ArrayList<Point3> matchList) {
+        final Scalar lineColor = new Scalar(255, 0, 0);
+        Point topLeft = new Point();
+        Point bottomRight = new Point();
+        for(Point3 p3 : matchList) {
+            topLeft.x = p3.x;
+            topLeft.y = p3.y;
+            bottomRight.x = topLeft.x + search.cols();
+            bottomRight.y = topLeft.y + search.rows();
+            Imgproc.rectangle(source, topLeft, bottomRight, lineColor, 5);
+        }
     }
 }
