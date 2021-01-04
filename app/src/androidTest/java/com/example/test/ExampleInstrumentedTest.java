2f1d0501c6f1a803f9edf34989992e4090517c3d
wangzhongdi <wangzhongdi@bbktel.com>
2021-01-04 16:09:58 +0800

    test j2v8

--- app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/test/ExampleInstrumentedTest.java
@@ -1,14 +1,16 @@
 package com.example.test;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.eclipsesource.v8.V8;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -23,5 +25,17 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.example.test", appContext.getPackageName());
+    }
+
+    @Test
+    public void j2v8Test() throws Exception {
+        String script = "var hello = 'hello';\n"
+                + "var world = 'world';\n"
+                + "hello.concat(world).length;";
+
+        V8 runtime = V8.createV8Runtime();
+        int result = runtime.executeIntegerScript(script);
+        runtime.close();
+        assertEquals(10, result);
     }
 }
--- app/src/main/java/com/example/test/empty/MyFragment.java
+++ app/src/main/java/com/example/test/empty/MyFragment.java
@@ -1,20 +1,26 @@
 package com.example.test.empty;
 
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.eclipsesource.v8.V8;
 import com.example.test.R;
 
 public class MyFragment extends Fragment implements MyContract.View {
+    private static final String TAG = "MyFragment";
 
     private MyContract.Presenter mPresenter;
 
+    private Button mBtn;
     private TextView mTextView;
 
     @Override
@@ -43,7 +49,42 @@ public class MyFragment extends Fragment implements MyContract.View {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.my_frag, container, false);
         mTextView = root.findViewById(R.id.my_frag_textView1);
+        mBtn = root.findViewById(R.id.my_test_btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTestBtnClicked();
+            }
+        });
         return root;
+    }
+
+    private void onTestBtnClicked() {
+        try {
+            testJ2v8();
+        } catch (Exception e) {
+            mTextView.setText(e.getMessage());
+            Log.e(TAG, "", e);
+        }
+    }
+
+    private void testJ2v8() {
+        StringBuilder sb = new StringBuilder();
+        ApplicationInfo appInfo = getContext().getApplicationInfo();
+        sb.append("app lib path=").append(appInfo.nativeLibraryDir).append("\n");
+        String script = "var hello = 'hello';\n"
+                + "var world = 'world';\n"
+                + "hello.concat(world).length;";
+
+        try {
+            V8 runtime = V8.createV8Runtime();
+            int result = runtime.executeIntegerScript(script);
+            sb.append("result=").append(result).append("\n");
+            runtime.close();
+        } catch (Throwable e) {
+            sb.append(e.getMessage()).append("\n");
+        }
+        mTextView.setText(sb.toString());
     }
 
     public static MyFragment newInstance() {
