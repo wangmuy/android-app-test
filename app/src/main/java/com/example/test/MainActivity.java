7fbf984eaac2d950bdb967a70da103b8f044527e
wangmuy <wangmuy@gmail.com>
2023-12-22 18:23:11 +0800

    update gradle

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.test;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
