961703d84f834a3b04a158f548e4545cf6a9eed4
wangmuy <wangmuy@gmail.com>
2024-07-22 18:01:54 +0800

    use eval

--- app/src/main/java/com/example/test/business/MainActivity.kt
+++ app/src/main/java/com/example/test/business/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.test.business
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.chaquo.python.Python
 import com.example.test.Const
@@ -13,6 +14,8 @@ class MainActivity: AppCompatActivity() {
         private const val TAG = "${Const.PREFIX}MainActivity"
     }
 
+    lateinit var pyCodeEdit: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,8 +23,15 @@ class MainActivity: AppCompatActivity() {
         findViewById<Button>(R.id.exePyBtn).setOnClickListener{v->
             onPyExe()
         }
+
+        findViewById<Button>(R.id.exePyDynBtn).setOnClickListener{v->
+            onPyDynExe()
+        }
+
+        pyCodeEdit = findViewById(R.id.pyCodeEdit)
     }
 
+    // python.stdout: helloPython from pyExe
     private fun onPyExe() {
         Thread {
             try {
@@ -29,7 +39,22 @@ class MainActivity: AppCompatActivity() {
                 py.getModule("main").callAttr("main")
                 Log.d(TAG, "pyExe end")
             } catch (e: Exception) {
-                Log.e(TAG, "", e)
+                Log.e(TAG, "pyExe", e)
+            }
+        }.start()
+    }
+
+    // python.stdout: helloPython from pyDynExe
+    private fun onPyDynExe() {
+        val pyCode = pyCodeEdit.text.toString()
+        Log.d(TAG, "onPyDynExe pyCode=$pyCode")
+        Thread {
+            try {
+                val py = Python.getInstance()
+                val ret = py.builtins.callAttr("eval", pyCode, py.builtins.callAttr("dict"))
+                Log.d(TAG, "pyDynExe end, ret=$ret")
+            } catch (e: Exception) {
+                Log.e(TAG, "pyDynExe", e)
             }
         }.start()
     }
